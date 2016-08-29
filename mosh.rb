b6aec492c73cff9ef43a54d7def522887364b2f4
Erin Sullivan <kittylily@gmail.com>
2016-08-28 20:50:05 -0700

    change name in mosh.rb

--- mosh.rb
+++ mosh.rb
@@ -1,6 +1,6 @@
 require_relative 'lib/avigif'
-require_relative 'lib/gifglitch'
+require_relative 'lib/gifmosh'
 
-cat_mosh = GifGlitch::Gif.new("cat_tube.gif")
+cat_mosh = GifMosh::Gif.new("cat_tube.gif")
 # AviGif.gif2avi 'cat_tube.gif', 'cat_tube.avi'
 # AviGif.avi2gif 'cat_tube.avi', 'cat_tube_out.gif'
