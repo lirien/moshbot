3bed7a065c2f0fa59d7c3214e3b19401c604b1aa
Erin Sullivan <kittylily@gmail.com>
2016-08-30 22:58:42 -0700

    :rotating_light: Configure rubocop

--- lib/gifmosh/gif.rb
+++ lib/gifmosh/gif.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 require 'aviglitch'
 require 'fileutils'
 require_relative 'avi'
--- lib/gifmosh/gifmosh.rb
+++ lib/gifmosh/gifmosh.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 require 'streamio-ffmpeg'
 require 'rmagick'
 require 'fileutils'
