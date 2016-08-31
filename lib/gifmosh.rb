1a54d434f845eb269416f4752ed38287b58deccc
Erin Sullivan <kittylily@gmail.com>
2016-08-30 21:16:32 -0700

    :shirt: Lint lib

--- lib/gifmosh.rb
+++ lib/gifmosh.rb
@@ -1,81 +1,76 @@
+# frozen_string_literal: true
 require 'aviglitch'
 require 'streamio-ffmpeg'
 require 'rmagick'
 require 'fileutils'
 
 module GifMosh
-    class Gif
-        def initialize gifpath
-            @basepath = File.basename(gifpath,File.extname(gifpath))
-            @extension = File.extname(gifpath)
-        end
-
-        def create_avi
-            GifMosh.gif2avi("#{@basepath}#{@extension}", "#{@basepath}.avi")
-            @video = AviGlitch.open "#{@basepath}.avi"
-            @pframes = []
-            @iframes = []
-
-            @length = @video.frames.size
-
-            @video.frames.each_with_index do |frame, index|
-              @pframes.push(index) if frame.is_pframe?
-              @iframes.push(index) if frame.is_iframe?
-            end
-        end
-
-        def remove_avis
-            FileUtils.rm "#{@basepath}.avi", :force => true
-            FileUtils.rm "#{@basepath}_out.avi", :force => true
-        end
-
-        def melt_from_frame frame,outpath="#{@basepath}_out.gif",repeat=20
-            if !File.exist? "#{@basepath}.avi"
-                create_avi
-            end
-            result = @video.frames[0, frame]
-            repeat.times do
-              result.concat(@video.frames[frame, 1])
-            end
-            avioutfile = AviGlitch.open result
-            avioutfile.output "#{@basepath}_out.avi"
-            GifMosh.avi2gif("#{@basepath}_out.avi", outpath)
-            remove_avis
-        end
-
-        def melt_from_random outpath="#{@basepath}_out.gif"
-            if !File.exist? "#{@basepath}.avi"
-                create_avi
-            end
-            melt_from_frame(@pframes.sample, outpath)
-        end
-
+  class Gif
+    def initialize(gifpath)
+      @basepath = File.basename(gifpath, File.extname(gifpath))
+      @extension = File.extname(gifpath)
     end
 
-    #Converts a gif (or mp4!) to an avi
-    def GifMosh.gif2avi inpath,outpath="./output.avi"
-        movie = FFMPEG::Movie.new(inpath)
-        options = {pix_fmt: "yuv420p",
-            custom: %w(-vf scale=trunc(iw/2)*2:trunc(ih/2)*2)}
-        movie.transcode(outpath, options)
+    def create_avi
+      GifMosh.gif2avi("#{@basepath}#{@extension}", "#{@basepath}.avi")
+      @video = AviGlitch.open "#{@basepath}.avi"
+      @pframes = []
+      @iframes = []
+
+      @length = @video.frames.size
+
+      @video.frames.each_with_index do |frame, index|
+        @pframes.push(index) if frame.is_pframe?
+        @iframes.push(index) if frame.is_iframe?
+      end
     end
 
-    #Converts an avi to a gif
-    def GifMosh.avi2gif inpath,outpath="./output.gif"
-        movie = FFMPEG::Movie.new(inpath)
-        Dir.mkdir("frames")
-        options = {flags: "lanczos", fps: 15,
-            custom: %w(-vf scale=320:-1:)}
-        transcoder_options = { validate: false }
-        movie.transcode("frames/ffout%03d.png", options, transcoder_options)
-
-        image = Magick::ImageList.new(*Dir.glob("frames/*"))
-        image.coalesce
-        image.optimize_layers Magick::OptimizeLayer
-        image.write(outpath)
-        FileUtils.rm_r Dir.glob("frames/*")
-        FileUtils.rmdir "frames"
-        return outpath
+    def remove_avis
+      FileUtils.rm "#{@basepath}.avi", force: true
+      FileUtils.rm "#{@basepath}_out.avi", force: true
     end
 
+    def melt_from_frame(frame, outpath = "#{@basepath}_out.gif", repeat = 20)
+      create_avi unless File.exist? "#{@basepath}.avi"
+      result = @video.frames[0, frame]
+      repeat.times do
+        result.concat(@video.frames[frame, 1])
+      end
+      avioutfile = AviGlitch.open result
+      avioutfile.output "#{@basepath}_out.avi"
+      GifMosh.avi2gif("#{@basepath}_out.avi", outpath)
+      remove_avis
+    end
+
+    def melt_from_random(outpath = "#{@basepath}_out.gif")
+      create_avi unless File.exist? "#{@basepath}.avi"
+      melt_from_frame(@pframes.sample, outpath)
+    end
+  end
+
+  # Converts a gif (or mp4!) to an avi
+  def self.gif2avi(inpath, outpath = './output.avi')
+    movie = FFMPEG::Movie.new(inpath)
+    options = { pix_fmt: 'yuv420p',
+                custom: %w(-vf scale=trunc(iw/2)*2:trunc(ih/2)*2) }
+    movie.transcode(outpath, options)
+  end
+
+  # Converts an avi to a gif
+  def self.avi2gif(inpath, outpath = './output.gif')
+    movie = FFMPEG::Movie.new(inpath)
+    Dir.mkdir('frames')
+    options = { flags: 'lanczos', fps: 15,
+                custom: %w(-vf scale=320:-1:) }
+    transcoder_options = { validate: false }
+    movie.transcode('frames/ffout%03d.png', options, transcoder_options)
+
+    image = Magick::ImageList.new(*Dir.glob('frames/*'))
+    image.coalesce
+    image.optimize_layers Magick::OptimizeLayer
+    image.write(outpath)
+    FileUtils.rm_r Dir.glob('frames/*')
+    FileUtils.rmdir 'frames'
+    outpath
+  end
 end
--- lib/moshbot.rb
+++ lib/moshbot.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
 require 'twitter'
 require 'json'
 
 module MoshBot
-    class Bot
-        attr_accessor :client
-        def initialize configpath="config.json"
-            configfile = File.read(configpath)
-            config_args = JSON.parse(configfile)
-            @client = Twitter::REST::Client.new do |config|
-              config.consumer_key        = config_args["consumer_key"]
-              config.consumer_secret     = config_args["consumer_secret"]
-              config.access_token        = config_args["access_token_key"]
-              config.access_token_secret = config_args["access_token_secret"]
-            end
-        end
+  class Bot
+    attr_accessor :client
+    def initialize(configpath = 'config.json')
+      configfile = File.read(configpath)
+      config_args = JSON.parse(configfile)
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = config_args['consumer_key']
+        config.consumer_secret     = config_args['consumer_secret']
+        config.access_token        = config_args['access_token_key']
+        config.access_token_secret = config_args['access_token_secret']
+      end
     end
+  end
 end
