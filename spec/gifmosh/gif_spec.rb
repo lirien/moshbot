d63711ce2dd9d9be2bedb41bdb16c49334bba0a3
Erin Sullivan <kittylily@gmail.com>
2016-09-12 16:11:49 -0700

    🎞 Add test for same fps on input and output

--- spec/gifmosh/gif_spec.rb
+++ spec/gifmosh/gif_spec.rb
@@ -5,22 +5,23 @@ require_relative '../../lib/gifmosh'
 describe GifMosh::Gif do
 end
 
-describe "#fps" do
+describe "#get_fps" do
   it 'returns the correct fps for mp4' do
     mp4 = GifMosh::Gif.new(fixture('good_day_sir.mp4'))
     expect(mp4.fps).to eq 10.18
   end
   it 'returns the correct fps for gif' do
-    gif = GifMosh::Gif.new(fixture('melted_cat.gif'))
-    expect(gif.fps).to eq 10
+    gif = GifMosh::Gif.new(fixture('cat_tube.gif'))
+    expect(gif.fps).to eq 12.5
   end
 end
 
-describe "#keep_fps" do
+describe "#same_fps" do
   it 'input and output have the same fps' do
-    #convert the mp4 to an avi
-    #convert the avi back to a gif
-    #confirm fps are the same
-    #destroy the created gif
+    gif = GifMosh::Gif.new(fixture('cat_tube.gif'))
+    out_gif = gif.melt
+    expect(gif.fps).to eq 12.5
+    expect(out_gif.fps).to eq 12.5
+    out_gif.destroy
   end
 end
