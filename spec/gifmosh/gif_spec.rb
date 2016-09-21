9b8448eae3f6c83a6cfc6639cce18865f95aade8
Erin Sullivan <kittylily@gmail.com>
2016-09-20 22:10:15 -0700

    👕 Rubocop gifspec

--- spec/gifmosh/gif_spec.rb
+++ spec/gifmosh/gif_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../lib/moshbot'
 require_relative '../../lib/gifmosh'
 
 describe GifMosh::Gif do
-  describe "#get_fps" do
+  describe '#get_fps' do
     it 'returns the correct fps for a mp4' do
       mp4 = GifMosh::Gif.new(fixture('good_day_sir.mp4'))
       expect(mp4.fps).to eq 10.18
@@ -15,7 +15,7 @@ describe GifMosh::Gif do
     end
   end
 
-  describe "#same_fps" do
+  describe '#same_fps' do
     it 'creates an output with the same fps' do
       gif = GifMosh::Gif.new(fixture('cat_tube.gif'))
       out_gif = gif.melt
@@ -24,16 +24,16 @@ describe GifMosh::Gif do
     end
   end
 
-  describe "#resize" do
+  describe '#resize' do
     it 'resizes avi to make gif with width 200' do
       gif = GifMosh::Gif.new(fixture('cat_tube.gif'))
       avi = gif.to_avi
-      smaller_gif = avi.to_gif(outpath: 'cat_tube_small.gif', width: 200 )
+      smaller_gif = avi.to_gif(outpath: 'cat_tube_small.gif', width: 200)
       expect(smaller_gif.width).to eq 200
       smaller_gif.destroy
     end
-    
-    it 'resizes itself to make its width 200' do
+
+    it 'resizes itself to make width 200' do
       gif = GifMosh::Gif.new(fixture('cat_tube.gif'))
       smaller_gif = gif.resize
       expect(smaller_gif.width).to eq 200
